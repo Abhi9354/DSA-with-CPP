f2389a536a299b35668de357fb88869b9c89297c
knowledge <abhi7ty4@gmail.com>
2024-03-17 22:44:18 +0530

    dsa

--- PrimeMath/primeNumber.cpp
+++ PrimeMath/primeNumber.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-int  x = 7;
+int  x = 36;
 int count = 0;
-for(int i = 2 ; i<x;i++) {
+for(int i = 2 ; i<=sqrt(x);i++) {
     if(x%i==0)count++;
 }   
 if (count>0)cout<<"not prime number";
