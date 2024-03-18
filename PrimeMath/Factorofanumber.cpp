9edb0f00fdb05e40efe0baa3c32f5d902a7613ae
knowledge <abhi7ty4@gmail.com>
2024-03-18 22:06:42 +0530

    Dsa

---
+++ PrimeMath/Factorofanumber.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printFactor(int n ){
+    for(int i = 1 ; i<=sqrt(n);i++){
+        if(n%i==0){
+        cout<<i<<" ";
+
+        cout<<n/i<<" ";
+
+        }
+    }
+}
+int main(){
+    int n = 60;
+    printFactor(n);
+}
