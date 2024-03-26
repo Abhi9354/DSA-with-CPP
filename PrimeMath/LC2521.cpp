71b57147b6b49e2fa7b15c5f25ce1f869cac96df
knowledge <abhi7ty4@gmail.com>
2024-03-26 17:40:25 +0530

    Dsa

--- PrimeMath/LC2521.cpp
+++ PrimeMath/LC2521.cpp
@@ -1,9 +1,12 @@
-
-
 #include <bits/stdc++.h>
 using namespace std;
+
+ void fillprime(vector<int>& sieve){
+         int n = sieve.size()-1;
+    for(int i =2 ; i*i<=n;i++)if(sieve[i]==1)for(int j = i*2 ; j<=n ; j+=i)sieve[j]=0;
+     }
 int main(){
     vector<int>nums;
     nums={2,4,3,7,10,6};
-    
+
 }
