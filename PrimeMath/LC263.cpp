19faeaea2166abe99d9c8c3741b0dae8c0eda0f7
knowledge <abhi7ty4@gmail.com>
2024-04-01 19:33:41 +0530

    Dsa

--- PrimeMath/LC263.cpp
+++ PrimeMath/LC263.cpp
@@ -1,16 +1,20 @@
 
-
 #include <bits/stdc++.h>
 using namespace std;
 
-
- bool isUgly(int n) {
-        if(n<=0)return false;
-        while(n%2==0)n/=2;
-        while(n%3==0)n/=3;
-        while(n%5==0)n/=5;
-         return (n==1);
-    }
-int main(){
-     cout<<isUgly(7);
+bool isUgly(int n)
+{
+     if (n <= 0)
+          return false;
+     while (n % 2 == 0)
+          n /= 2;
+     while (n % 3 == 0)
+          n /= 3;
+     while (n % 5 == 0)
+          n /= 5;
+     return (n == 1);
+}
+int main()
+{
+     cout << isUgly(7);
 }
