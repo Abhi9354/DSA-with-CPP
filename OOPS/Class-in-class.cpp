bb8eafcfe420cbcbb797ba3805e99006d4a28cfc
knowledge <abhi7ty4@gmail.com>
2024-04-11 13:25:10 +0530

    Dsa

--- OOPS/Class-in-class.cpp
+++ OOPS/Class-in-class.cpp
@@ -3,46 +3,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Gun{
-private:
-int ammo;
-int damage;
+class Gun
+{
+public:
+    int ammo;
+    int damage;
 };
-class Player{
-    private:
+class Player
+{
+private:
     int health;
     int age;
-    Gun gun;  //class ke ander class banayenge
-    public:
-    void setHealth(int health){
+    Gun gun; // class ke ander object banayenge
+    class Helmet
+    {
+        private:
+        int health;
+        public:
+        void setHealth(int health)
+        {
+            this->health = health;
+        }
+        int getHealth()
+        {
+            return health;
+        }   
+    };
+
+
+public:
+    void setHealth(int health)
+    {
         this->health = health;
     }
-     void setAge(int age){
+    void setAge(int age)
+    {
         this->age = age;
     }
-    int getHealth(){
+    int getHealth()
+    {
         return health;
     }
-    int getAge(){
+    int getAge()
+    {
         return age;
     }
 
-    Gun getGun(){
+    Gun getGun()
+    {
         return gun;
     }
 
-    void setGun(Gun gun){
-        this->gun=gun;
+    void setGun(Gun gun)
+    {
+        this->gun = gun;
     }
 };
-int main(){
+int main()
+{
     Gun Akm;
-   
+    Akm.ammo = 10;
+    Akm.damage = 100;
+
     Player Abhishek;
     Abhishek.setHealth(100);
     Abhishek.setAge(20);
-    cout<<Abhishek.getHealth();
-    
-   
+    cout << Abhishek.getHealth() << endl;
+    cout << Abhishek.getAge() << endl;
+
+    Abhishek.setGun(Akm); // passing object as parameter
+    cout << Abhishek.getGun().ammo << endl;
+    cout << Abhishek.getGun().damage << endl;
 }
