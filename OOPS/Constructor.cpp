6df33742953c6a8f9aee6514af961c772f48fecf
knowledge <abhi7ty4@gmail.com>
2024-04-15 23:44:12 +0530

    Dsa

---
+++ OOPS/Constructor.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Bike{
+    public:
+    int tyresize;
+Bike(){
+    cout<<"hello constructor call"<<endl;
+}
+
+};
+int main(){
+    Bike tvs;
+}
